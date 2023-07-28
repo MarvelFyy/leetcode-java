edb5de45fa08d98cfa68f7226dbb29c58657b3e1
MarvelFyy <751594278@qq.com>
2023-07-29 07:43:43 +0800

    day03-2

---
+++ src/相交链表.java
@@ -0,0 +1,8 @@
+import org.w3c.dom.ls.LSInput;
+
+public class 相交链表 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p = null;
+        return p;
+    }
+}
